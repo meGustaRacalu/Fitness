ac801f3f0002877d2a31a5a390b6fd078f65cd3c
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-12-18 14:31:07 -0300

    @Repository

--- src/main/java/com/gen/fitness/repository/CategoriaRepository.java
+++ src/main/java/com/gen/fitness/repository/CategoriaRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.gen.fitness.model.CategoriaModel;
 
+@Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaModel, Long> {
 
     List<CategoriaModel> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
