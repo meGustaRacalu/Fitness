e4a5c8737549d306acb11582127e89d9d55b26e8
Gustav0Felipe <gustavo.custodio55@hotmail.com>
2024-12-18 16:11:36 -0300

    UserDetails Changes.

--- src/main/java/com/gen/fitness/security/UserDetailsImpl.java
+++ src/main/java/com/gen/fitness/security/UserDetailsImpl.java
@@ -38,4 +38,24 @@ public class UserDetailsImpl implements UserDetails{
 	public String getPassword() {
 		return password;
 	}
+	
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
